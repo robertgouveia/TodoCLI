63247eba2d40a71fce5f3ea20da146a63b9d6e18
robertgouveia <robertgouveia03@hotmail.com>
2024-08-26 12:14:01 +0100

    Flags

--- cmd/root.go
+++ cmd/root.go
@@ -3,15 +3,26 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
 
+// defining a flag
+var shout bool
+
 var rootCmd = &cobra.Command{
 	Use:   "todo-cli",
 	Short: "A Simple Todo CLI",
+	Args:  cobra.MinimumNArgs(1), // only allow 1 or more arguments
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("Hello World")
+		greeting := fmt.Sprintf("Hello, %s\n", args[0])
+
+		if shout {
+			greeting = strings.ToUpper(greeting)
+		}
+
+		fmt.Println(greeting)
 	},
 }
 
@@ -20,4 +31,9 @@ func Execute() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+}
+
+func init() {
+	//init runs before the command is executed
+	rootCmd.Flags().BoolVarP(&shout, "shout", "s", false, "Uppercase")
 }
