09488e3fdd31236fd8ba9b50cc4d820c5595a57b
robertgouveia <robertgouveia03@hotmail.com>
2024-08-26 12:19:54 +0100

    Multi File CLI

--- cmd/root.go
+++ cmd/root.go
@@ -2,10 +2,8 @@ package cmd
 
 import (
 	"fmt"
-	"os"
-	"strings"
-
 	"github.com/spf13/cobra"
+	"os"
 )
 
 // defining a flag
@@ -14,15 +12,8 @@ var shout bool
 var rootCmd = &cobra.Command{
 	Use:   "todo-cli",
 	Short: "A Simple Todo CLI",
-	Args:  cobra.MinimumNArgs(1), // only allow 1 or more arguments
 	Run: func(cmd *cobra.Command, args []string) {
-		greeting := fmt.Sprintf("Hello, %s\n", args[0])
-
-		if shout {
-			greeting = strings.ToUpper(greeting)
-		}
-
-		fmt.Println(greeting)
+		fmt.Println("Todo CLI")
 	},
 }
 
@@ -31,9 +22,4 @@ func Execute() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-}
-
-func init() {
-	//init runs before the command is executed
-	rootCmd.Flags().BoolVarP(&shout, "shout", "s", false, "Uppercase")
 }
